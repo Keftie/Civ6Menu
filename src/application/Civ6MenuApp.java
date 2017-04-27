547773283df89fa75f4044e461f6353f31e93e6b
Kevin <kevinvdbosch_1@hotmail.com>
2017-04-27 11:11:59 +0200

    Created menu entrypoint

---
+++ src/application/Civ6MenuApp.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Civ6MenuApp extends Application {
+
+	@Override
+	public void start(Stage PrimaryStage) throws Exception {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
